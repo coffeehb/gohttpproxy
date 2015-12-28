1ee703f5d574b5ae380debcb3a9b510010f501e0
ZhiFeng Hu <hufeng1987@gmail.com>
2015-12-28 17:09:57 +0800

    Update timeout
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- main.go
+++ main.go
@@ -5,17 +5,22 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
 func main() {
 	log.Print("Starting proxy")
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Tr.Dial = func(network, addr string) (c net.Conn, err error) {
-		c, err = net.Dial(network, addr)
+		c, err = (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 10 * time.Second,
+		}).Dial(network, addr)
 		if c, ok := c.(*net.TCPConn); err == nil && ok {
 			log.Println("Set keep alive")
 			c.SetKeepAlive(true)
 			c.SetNoDelay(true)
+			c.SetDeadline(time.Now().Add(30 * time.Second))
 		}
 		return
 	}
