3094a41e78d3ac45c8d45e8e6a6e2c177f8670ca
ZhiFeng Hu <hufeng1987@gmail.com>
2016-01-14 14:40:32 +0800

    Update host and port
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"github.com/elazarl/goproxy"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
+	var host string
+	var port int
+	args := os.Args[1:]
+	argslen := len(args)
+	fmt.Println(argslen)
+	host = "127.0.0.1"
+	port = 8123
+	if argslen >= 1 {
+		host = args[0]
+	}
+	if argslen >= 2 {
+		newport, err := strconv.Atoi(args[1])
+		if err != nil {
+			port = 53
+		} else {
+			port = newport
+		}
+	}
 	log.Print("Start proxy")
 	proxy := goproxy.NewProxyHttpServer()
 	to := 3 * time.Second
@@ -25,5 +46,5 @@ func main() {
 		return
 	}
 	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe("127.0.0.1:8123", proxy))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), proxy))
 }
