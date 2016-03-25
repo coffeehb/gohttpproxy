20422932c4b1e48523363313089d7524ea08a8aa
ZhiFeng Hu <hufeng1987@gmail.com>
2016-03-25 13:57:27 +0800

    Remove keepalive
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- main.go
+++ main.go
@@ -24,27 +24,12 @@ func main() {
 	}
 	if argslen >= 2 {
 		newport, err := strconv.Atoi(args[1])
-		if err != nil {
-			port = 53
-		} else {
+		if err == nil {
 			port = newport
 		}
 	}
 	log.Print("Start proxy")
 	proxy := goproxy.NewProxyHttpServer()
-	to := 3 * time.Second
-	proxy.Tr.Dial = func(network, addr string) (c net.Conn, err error) {
-		c, err = (&net.Dialer{
-			Timeout:   to,
-			KeepAlive: to,
-		}).Dial(network, addr)
-		if c, ok := c.(*net.TCPConn); err == nil && ok {
-			log.Println("Set keep alive")
-			c.SetKeepAlive(true)
-			c.SetNoDelay(true)
-		}
-		return
-	}
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), proxy))
 }
