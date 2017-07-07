b1f5f628194be45414cbf5da39072ba31a6a6118
netroby <netroby@users.noreply.github.com>
2017-07-07 17:24:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,58 +7,87 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
-	"path/filepath"
+	"path"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/google/martian"
+	mapi "github.com/google/martian/api"
 	"github.com/google/martian/cors"
+	"github.com/google/martian/fifo"
 	"github.com/google/martian/har"
 	"github.com/google/martian/httpspec"
 	"github.com/google/martian/marbl"
 	"github.com/google/martian/martianhttp"
 	"github.com/google/martian/martianlog"
 	"github.com/google/martian/mitm"
+	"github.com/google/martian/servemux"
 	"github.com/google/martian/trafficshape"
 	"github.com/google/martian/verify"
 
 	_ "github.com/google/martian/body"
 	_ "github.com/google/martian/cookie"
-	mlog "github.com/google/martian/log"
+	_ "github.com/google/martian/failure"
 	_ "github.com/google/martian/martianurl"
 	_ "github.com/google/martian/method"
 	_ "github.com/google/martian/pingback"
+	_ "github.com/google/martian/port"
 	_ "github.com/google/martian/priority"
 	_ "github.com/google/martian/querystring"
+	_ "github.com/google/martian/skip"
+	_ "github.com/google/martian/stash"
+	_ "github.com/google/martian/static"
 	_ "github.com/google/martian/status"
 )
 
 var (
-	level          = flag.Int("v", 0, "log level")
 	addr           = flag.String("addr", ":8080", "host:port of the proxy")
+	apiAddr        = flag.String("api-addr", ":8181", "host:port of the configuration API")
 	tlsAddr        = flag.String("tls-addr", ":4443", "host:port of the proxy over TLS")
 	api            = flag.String("api", "martian.proxy", "hostname for the API")
 	generateCA     = flag.Bool("generate-ca-cert", false, "generate CA certificate and private key for MITM")
-	cert           = flag.String("cert", "", "CA certificate used to sign MITM certificates")
-	key            = flag.String("key", "", "private key of the CA used to sign MITM certificates")
+	cert           = flag.String("cert", "", "filepath to the CA certificate used to sign MITM certificates")
+	key            = flag.String("key", "", "filepath to the private key of the CA used to sign MITM certificates")
 	organization   = flag.String("organization", "Martian Proxy", "organization name for MITM certificates")
 	validity       = flag.Duration("validity", time.Hour, "window of time that MITM certificates are valid")
 	allowCORS      = flag.Bool("cors", false, "allow CORS requests to configure the proxy")
 	harLogging     = flag.Bool("har", false, "enable HAR logging API")
+	marblLogging   = flag.Bool("marbl", false, "enable MARBL logging API")
 	trafficShaping = flag.Bool("traffic-shaping", false, "enable traffic shaping API")
 	skipTLSVerify  = flag.Bool("skip-tls-verify", false, "skip TLS server verification; insecure")
+	dsProxyURL     = flag.String("downstream-proxy-url", "", "URL of downstream proxy")
 )
 
 func main() {
-	flag.Parse()
-
-	mlog.SetLevel(*level)
-
 	p := martian.NewProxy()
+	defer p.Close()
 
-	// Respond with 404 to any unknown proxy path.
-	http.HandleFunc(*api+"/", http.NotFound)
+	tr := &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: time.Second,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: *skipTLSVerify,
+		},
+	}
+	p.SetRoundTripper(tr)
+
+	if *dsProxyURL != "" {
+		u, err := url.Parse(*dsProxyURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		p.SetDownstreamProxy(u)
+	}
+
+	mux := http.NewServeMux()
 
 	var x509c *x509.Certificate
 	var priv interface{}
@@ -96,7 +125,7 @@ func main() {
 
 		// Expose certificate authority.
 		ah := martianhttp.NewAuthorityHandler(x509c)
-		configure("/authority.cer", ah)
+		configure("/authority.cer", ah, mux)
 
 		// Start TLS listener for transparent MITM.
 		tl, err := net.Listen("tcp", *tlsAddr)
@@ -108,8 +137,30 @@ func main() {
 	}
 
 	stack, fg := httpspec.NewStack("martian")
-	p.SetRequestModifier(stack)
-	p.SetResponseModifier(stack)
+
+	// wrap stack in a group so that we can forward API requests to the API port
+	// before the httpspec modifiers which include the via modifier which will
+	// trip loop detection
+	topg := fifo.NewGroup()
+
+	// Redirect API traffic to API server.
+	if *apiAddr != "" {
+		apip := strings.Replace(*apiAddr, ":", "", 1)
+		port, err := strconv.Atoi(apip)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Forward traffic that pattern matches in http.DefaultServeMux
+		apif := servemux.NewFilter(mux)
+		apif.SetRequestModifier(mapi.NewForwarder("", port))
+		topg.AddRequestModifier(apif)
+	}
+	topg.AddRequestModifier(stack)
+	topg.AddResponseModifier(stack)
+
+	p.SetRequestModifier(topg)
+	p.SetResponseModifier(topg)
 
 	m := martianhttp.NewModifier()
 	fg.AddRequestModifier(m)
@@ -117,11 +168,17 @@ func main() {
 
 	if *harLogging {
 		hl := har.NewLogger()
-		stack.AddRequestModifier(hl)
-		stack.AddResponseModifier(hl)
+		muxf := servemux.NewFilter(mux)
+		// Only append to HAR logs when the requests are not API requests,
+		// that is, they are not matched in http.DefaultServeMux
+		muxf.RequestWhenFalse(hl)
+		muxf.ResponseWhenFalse(hl)
 
-		configure("/logs", har.NewExportHandler(hl))
-		configure("/logs/reset", har.NewResetHandler(hl))
+		stack.AddRequestModifier(muxf)
+		stack.AddResponseModifier(muxf)
+
+		configure("/logs", har.NewExportHandler(hl), mux)
+		configure("/logs/reset", har.NewResetHandler(hl), mux)
 	}
 
 	logger := martianlog.NewLogger()
@@ -130,31 +187,33 @@ func main() {
 	stack.AddRequestModifier(logger)
 	stack.AddResponseModifier(logger)
 
-	lsh := marbl.NewHandler()
-	http.Handle("/binlogs", lsh)
+	if *marblLogging {
+		lsh := marbl.NewHandler()
+		lsm := marbl.NewModifier(lsh)
+		muxf := servemux.NewFilter(mux)
+		muxf.RequestWhenFalse(lsm)
+		muxf.ResponseWhenFalse(lsm)
+		stack.AddRequestModifier(muxf)
+		stack.AddResponseModifier(muxf)
 
-	lsm := marbl.NewModifier(lsh)
-	stack.AddRequestModifier(lsm)
-	stack.AddResponseModifier(lsm)
-
-	// Proxy specific handlers.
-	// These handlers take precedence over proxy traffic and will not be
-	// intercepted.
+		// retrieve binary marbl logs
+		mux.Handle("/binlogs", lsh)
+	}
 
 	// Configure modifiers.
-	configure("/configure", m)
+	configure("/configure", m, mux)
 
 	// Verify assertions.
 	vh := verify.NewHandler()
 	vh.SetRequestVerifier(m)
 	vh.SetResponseVerifier(m)
-	configure("/verify", vh)
+	configure("/verify", vh, mux)
 
 	// Reset verifications.
 	rh := verify.NewResetHandler()
 	rh.SetRequestVerifier(m)
 	rh.SetResponseVerifier(m)
-	configure("/verify/reset", rh)
+	configure("/verify/reset", rh, mux)
 
 	l, err := net.Listen("tcp", *addr)
 	if err != nil {
@@ -164,14 +223,21 @@ func main() {
 	if *trafficShaping {
 		tsl := trafficshape.NewListener(l)
 		tsh := trafficshape.NewHandler(tsl)
-		configure("/shape-traffic", tsh)
+		configure("/shape-traffic", tsh, mux)
 
 		l = tsl
 	}
 
-	log.Println("martian: proxy started on:", l.Addr())
+	lAPI, err := net.Listen("tcp", *apiAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("martian: starting proxy on %s and api on %s", l.Addr().String(), lAPI.Addr().String())
 
 	go p.Serve(l)
+
+	go http.Serve(lAPI, mux)
 
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Interrupt, os.Kill)
@@ -181,12 +247,21 @@ func main() {
 	log.Println("martian: shutting down")
 }
 
+func init() {
+	martian.Init()
+}
+
 // configure installs a configuration handler at path.
-func configure(path string, handler http.Handler) {
+func configure(pattern string, handler http.Handler, mux *http.ServeMux) {
 	if *allowCORS {
 		handler = cors.NewHandler(handler)
 	}
 
-	http.Handle(filepath.Join(*api, path), handler)
+	// register handler for martian.proxy to be forwarded to
+	// local API server
+	mux.Handle(path.Join(*api, pattern), handler)
 
+	// register handler for local API server
+	p := path.Join("localhost"+*apiAddr, pattern)
+	mux.Handle(p, handler)
 }
